75cbbf88f5ef5af2cc6188831d02a161016f7df2
Felipe Santos <f3l1p3mss@gmail.com>
2026-03-17 20:13:07 -0300

    estrutura inical de score de investimentos criada

---
+++ src/core/score.js
@@ -0,0 +1,10 @@
+// TODO: Implementar lógica de cálculo de score da carteira
+// Essa função será responsável por avaliar:
+// - Diversificação dos ativos
+// - Nível de concentração
+// - Distribuição percentual da carteira
+// O retorno esperado será um número representando o score da carteira (0 a 10)
+
+function calcularScoreCarteira(ativos) {
+  // Implementação futura
+}
