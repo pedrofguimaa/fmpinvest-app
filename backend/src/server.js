7b28c448d93e2dd19d252df0631bbba77178e526
Felipe Santos <f3l1p3mss@gmail.com>
2026-05-11 16:03:58 -0300

    feat: altera totalmente a interface, modernizando ela, deixando mais interativa e adiciona animações

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 ﻿const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const frontendPath = path.join(__dirname, '..', '..', 'frontend');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(frontendPath));
 app.use(routes);
 
 app.use((_req, res) => {
